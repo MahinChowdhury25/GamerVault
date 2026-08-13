08ea153d19b928cbb774e5f80d6f6df8b92fe33a
MahinChowdhury25 <22201158@uap-bd.edu>
2026-08-13 23:19:58 +0600

    Add user authentication

---
+++ games/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from .views import (
+    dashboard_view,
+    login_view,
+    logout_view,
+    register_view,
+)
+
+urlpatterns = [
+    path("", dashboard_view, name="dashboard"),
+    path("register/", register_view, name="register"),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+]
